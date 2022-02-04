c27ea6faf0134d6a583d54179401930d61cf505f
shawticus <shawmakesmagic@gmail.com>
2022-02-03 16:33:54 -0800

    Initialize with agents and all models

--- core/types.ts
+++ core/types.ts
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import { Component, Connection, Input, Output, NodeEditor } from 'rete'
+import { Component, Connection, Input, NodeEditor, Output } from 'rete'
 import { Node } from 'rete/types'
 //@seang todo: convert inspector plugin fully to typescript
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -10,13 +10,13 @@ import {
   WorkerOutputs,
 } from 'rete/types/core/data'
 
+import { EngineContext } from './src/engine'
+import { ThothConsole } from './src/plugins/debuggerPlugin/ThothConsole'
 import { Inspector } from './src/plugins/inspectorPlugin/Inspector'
 import { ModuleGraphData } from './src/plugins/modulePlugin/module-manager'
 import { TaskOutputTypes } from './src/plugins/taskPlugin/task'
 import { SocketNameType, SocketType } from './src/sockets'
-import { EngineContext } from './src/engine'
 import { ThothTask } from './src/thoth-component'
-import { ThothConsole } from './src/plugins/debuggerPlugin/ThothConsole'
 
 export type EventsTypes = {
   run: void
@@ -49,7 +49,7 @@ export type DataSocketType = {
 export type ThothNode = Node & {
   inspector: Inspector
   display: (content: string) => void
-  outputs: { name: string; [key: string]: unknown }[]
+  outputs: { name: string;[key: string]: unknown }[]
   category?: string
   deprecated?: boolean
   displayName?: string
@@ -178,9 +178,9 @@ export type WorkerReturn =
   | Promise<never[] | { entities: { name: string; type: string }[] }>
   | Promise<{ element: unknown } | undefined>
   | Promise<
-      | { result: { error: unknown; [key: string]: unknown } }
-      | { result?: undefined }
-    >
+    | { result: { error: unknown;[key: string]: unknown } }
+    | { result?: undefined }
+  >
   | Promise<{ text: unknown }>
   | Promise<{ boolean: boolean }>
   | Promise<null | undefined>
@@ -201,11 +201,11 @@ export type ThothWorker = (
 
 export interface PubSubBase
   extends CountSubscriptions,
-    ClearAllSubscriptions,
-    GetSubscriptions,
-    Publish,
-    Subscribe,
-    Unsubscribe {
+  ClearAllSubscriptions,
+  GetSubscriptions,
+  Publish,
+  Subscribe,
+  Unsubscribe {
   name: string
   version: string
 }
