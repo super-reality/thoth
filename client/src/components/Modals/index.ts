e9ec9300931664335d132b328524848b617f85b6
Heena Trivedi <trivediheenalove@gmail.com>
2022-04-12 18:45:05 +0530

    make changes in document

--- client/src/components/Modals/index.ts
+++ client/src/components/Modals/index.ts
@@ -3,6 +3,7 @@ import InfoModal from './InfoModal'
 import DeployModal from './DeployModal'
 import EditSpellModal from './EditSpellModal'
 import SaveAsModal from './SaveAsModal'
+import DocumentAddModal from './DocumentAddModal'
 
 const modals = {
   example: ExampleModal,
@@ -10,6 +11,7 @@ const modals = {
   deployModal: DeployModal,
   editSpellModal: EditSpellModal,
   saveAsModal: SaveAsModal,
+  documentAddModal: DocumentAddModal,
 }
 
 export const getModals = () => {
